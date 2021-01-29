03e6315bee855f834896479a361ff2c70f924290
rwtrecs <adolfo@rewrite.com.br>
2021-01-29 08:27:01 -0300

    main - 161/ conclude

---
+++ 161/script.js
@@ -0,0 +1,21 @@
+// var variavel1 = prompt('Digite algum número: ');
+// var variavel2 = prompt('Digite outro número: ');
+
+var variavel1 = 10;
+var variavel2 = 30;
+
+// parseInt
+// variavel1 = parseInt(variavel1);
+// variavel2 = parseInt(variavel2);
+
+// parseFloat
+// variavel1 = parseFloat(variavel1);
+// variavel2 = parseFloat(variavel2);
+
+// console.log(variavel1);
+// console.log(variavel2);
+
+// document.write(variavel1 + variavel2);
+
+// toString
+document.write(variavel1.toString() + variavel2.toString());
