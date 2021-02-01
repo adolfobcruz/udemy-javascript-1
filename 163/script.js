86583c824234a5e4129ce020ac5df76bcd45e151
rwtrecs <adolfo@rewrite.com.br>
2021-02-01 09:50:05 -0300

    main - 161/ continue

---
+++ 163/script.js
@@ -0,0 +1,51 @@
+// E (&&)
+// só retorna Verdadeiro se todas Expressões forem verdadeiras
+// com uma Expressão falsa, tudo é Falso
+
+// true && true && false = false
+// if (2 == 2 && 3 >= 4 && 'a' == 'b') {
+//   document.write('Verdadeiro!');
+// } else {
+//   document.write('Falso!');
+// }
+
+
+//
+
+
+
+// OU (||)
+// se somente uma Expressão for verdadeira, retorna Verdadeiro
+
+// true || false || false = true
+// if (2 == 2 || 3 >= 4 || 'a' == 'b') {
+//   document.write('Verdadeiro!');
+// } else {
+//   document.write('Falso!');
+// }
+
+
+
+//
+
+
+
+// NEGAÇÃO (!)
+// inverte o valor da Expressão
+// se for true se torna false
+// se for false se torna true
+
+// false se torna true = true
+// if (!(2 == 3)) {
+//   document.write('Verdadeiro!');
+// } else {
+//   document.write('Falso!');
+// }
+
+
+
+//
+
+
+
+// CONTINUAR - média do aluno
