097c30ef64a44d6df44964f0e6185c29440bae6d
rwtrecs <adolfo@rewrite.com.br>
2021-02-03 14:56:58 -0300

    main - 169/ continue

---
+++ 169/script.js
@@ -0,0 +1,14 @@
+// operadores aritméticos
+var num1 = 9;
+var num2 = 2;
+
+// document.write(num1 + num2);
+
+document.write('A soma entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 + num2) + '<br>');
+document.write('A subtração entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 - num2) + '<br>');
+document.write('A multiplicação entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 * num2) + '<br>');
+document.write('A divisão entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 / num2) + '<br>');
+document.write('O módulo entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 % num2) + '<br>');
+document.write('O incremento de ' + num1 + ' é: ' + (num1++) + '<br>');
+document.write(num1 + '<br>');
+document.write('O incremento de ' + num1 + ' é: ' + (++num1) + '<br>');
