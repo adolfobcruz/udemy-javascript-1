95928570441265521be37f7123c888219ac60c70
rwtrecs <adolfo@rewrite.com.br>
2021-02-03 09:02:44 -0300

    main - 165/ and 167/ conclude

---
+++ 165/script.js
@@ -0,0 +1,23 @@
+// 1) Crie uma aplicação capaz de identificar a faixa etária com base na idade informada pelo usuário. Considere os seguintes critérios:
+// Se a idade informada for maior ou igual a 0 e menor que 15, exibir a mensagem “Criança”.
+// Se a idade informada for maior ou igual a 15 e menor que 30, exibir a mensagem “Jovem”.
+// Se a idade informada for maior ou igual a 30 e menor que 60, exibir a mensagem “Adulto”.
+// Se a idade informada for maior ou igual a 60, exibir a mensagem “Idoso”.
+// Fique à vontade para utilizar qualquer uma das funções aprendidas para exibição de dados para o usuário.
+
+// alerta inicial
+alert('Vamos identificar a faixa etária de alguém?');
+
+// prompt pra digitar a idade
+var idade = prompt('Digite a idade da pessoa: ');
+
+// condições
+if (idade >= 0 && idade < 15) {
+  document.write('A faixa etária digitada é de uma: Criança!');
+} else if (idade >= 15 && idade < 30) {
+  document.write('A faixa etária digitada é de um: Jovem!');
+} else if (idade >= 30 && idade < 60) {
+  document.write('A faixa etária digitada é de um: Adulto!');
+} else if (idade >= 60) {
+  document.write('A faixa etária digitada é de um: Idoso!');
+}
