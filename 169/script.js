f3e0ebfb5e1e8f9660174378d2aaf6215cc28972
rwtrecs <adolfo@rewrite.com.br>
2021-02-04 08:56:34 -0300

    main - 169/ and 170/ conclude

--- 169/script.js
+++ 169/script.js
@@ -1,14 +1,20 @@
 // operadores aritméticos
-var num1 = 9;
-var num2 = 2;
+var num1 = prompt('Digite o primeiro número:');
+var num2 = prompt('Digite o segundo número:');
 
-// document.write(num1 + num2);
+var num1 = parseInt(num1);
+var num2 = parseInt(num2);
 
 document.write('A soma entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 + num2) + '<br>');
 document.write('A subtração entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 - num2) + '<br>');
 document.write('A multiplicação entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 * num2) + '<br>');
 document.write('A divisão entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 / num2) + '<br>');
-document.write('O módulo entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 % num2) + '<br>');
+document.write('O módulo entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 % num2) + '<br><br>');
+
 document.write('O incremento de ' + num1 + ' é: ' + (num1++) + '<br>');
 document.write(num1 + '<br>');
-document.write('O incremento de ' + num1 + ' é: ' + (++num1) + '<br>');
+document.write('O incremento de ' + num1 + ' é: ' + (++num1) + '<br><br>');
+
+document.write('O decremento de ' + num1 + ' é: ' + (num1--) + '<br>');
+document.write(num1 + '<br>');
+document.write('O decremento de ' + num1 + ' é: ' + (--num1) + '<br>');
---
+++ 170/script.js
@@ -0,0 +1,23 @@
+// operadores aritméticos na atribuição de valores
+var num1 = 10;
+
+num1 += 5;
+document.write(num1 + '<br>');
+
+num1 -= 5;
+document.write(num1 + '<br>');
+
+num1 *= 5;
+document.write(num1 + '<br>');
+
+num1 /= 5;
+document.write(num1 + '<br>');
+
+num1 %= 2;
+document.write(num1 + '<br><br>');
+
+
+var str1 = 'Hello ';
+
+str1 += 'World!';
+document.write(str1 + '<br>');
