1e220fda905ceb977d5e5c4ccd803d48606ffd37
Adolfo Cruz <adolfo@rewrite.com.br>
2022-01-14 11:51:54 -0400

    main - 180/

---
+++ 180/script.js
@@ -0,0 +1,35 @@
+var nome = ' Jorge Sant Ana ';
+
+// Propriedade:
+// length
+document.write(nome.length);
+document.write('<hr>');
+
+// Método:
+// charAt()
+document.write(nome.charAt(7));
+document.write('<hr>');
+
+// indexOf()
+document.write(nome.indexOf('i'));
+document.write('<hr>');
+
+// replace()
+document.write(nome.replace('Sant Ana', 'Silva'));
+document.write('<hr>');
+
+// substr()
+document.write(nome.substr(6, 4));
+document.write('<hr>');
+
+// toLowerCase
+document.write(nome.toLowerCase());
+document.write('<hr>');
+
+// toUpperCase
+document.write(nome.toUpperCase());
+document.write('<hr>');
+
+// trim
+document.write('-' + nome.trim() + '-');
+document.write('<hr>');
